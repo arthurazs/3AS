3871f1932ff0b4a3088417feacf8bd16b880be5b
Arthur Zopellaro <arthurazsoares@gmail.com>
2019-04-18 19:37:09 -0300

    Update hostapd to work with Ryu, Add run.sh

---
+++ experiment/run.sh
@@ -0,0 +1,25 @@
+clear
+
+echo ">>> Killing ryu and mininet"
+pkill ryu-manager
+pkill mn
+
+echo ">>> Cleaning mininet"
+sudo mn -c
+
+echo ">>> Removing DNS"
+sudo systemctl disable avahi-daemon
+sudo service avahi-daemon stop
+
+# run experiment
+echo ">>> Cleaning logs"
+sudo rm -r logs/*
+
+echo ">>> Running ryu"
+ryu-manager example_switch_13.py > logs/ryu.log 2>&1 &
+echo ">>> Running mininet"
+sudo python topo.py > logs/mininet.log 2>&1
+
+# kill pox
+echo ">>> Killing ryu"
+pkill ryu-manager
