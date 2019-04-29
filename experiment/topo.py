4f777d8b87ca25289aa385d0b218ec1a9b571235
Arthur Zopellaro <arthurazsoares@gmail.com>
2019-04-29 16:03:48 -0300

    Add new host to authenticate into the network

--- experiment/topo.py
+++ experiment/topo.py
@@ -8,14 +8,17 @@ class Simple_Topology(Topo):
     def __init__(self):
         Topo.__init__(self)
 
-        h1 = self.addHost('h1', ip='10.0.0.1/24', mac='00:00:00:00:00:01')
+        h1 = self.addHost(  # TODO Improve
+            'h1', ip='10.0.0.1/24', mac='00:00:00:00:00:01', inNamespace=False)
         h2 = self.addHost('h2', ip='10.0.0.2/24', mac='00:00:00:00:00:02')
+        h3 = self.addHost('h3', ip='10.0.0.3/24', mac='00:00:00:00:00:03')
         s1 = self.addSwitch('s1')
         s2 = self.addSwitch('s2')
 
         self.addLink(h1, s1)
         self.addLink(s1, s2)
         self.addLink(s2, h2)
+        self.addLink(s1, h3)
 
 
 def main():
@@ -23,7 +26,7 @@ def main():
     mn = Mininet(topo=topo, controller=None)
     mn.addController(
         'c0', controller=RemoteController, ip='127.0.0.1', port=6653)
-    h1, h2, s1, s2 = mn.get('h1', 'h2', 's1', 's2')
+    auth, ev, scada, s1, s2 = mn.get('h1', 'h2', 'h3', 's1', 's2')
 
     s1.cmd('rm -rf /var/run/wpa_supplicant')
 
@@ -39,28 +42,37 @@ def main():
         sw.cmd("sysctl -w net.ipv6.conf.default.disable_ipv6=1")
         sw.cmd("sysctl -w net.ipv6.conf.lo.disable_ipv6=1")
 
-    h2.cmd('tcpdump -i h2-eth0 -w logs/h2.pcap &')
-    h1.cmd('tcpdump -i h1-eth0 -w logs/h1.pcap &')
-    h1.cmd('tcpdump -i lo -w logs/h1-lo.pcap &')
+    ev.cmd('tcpdump -i h2-eth0 -w logs/ev.pcap &')
+    auth.cmd('tcpdump -i h1-eth0 -w logs/auth.pcap &')
+    auth.cmd('tcpdump -i lo -w logs/auth-lo.pcap &')
     s1.cmd('tcpdump -i lo -w logs/s1-lo.pcap &')
+    scada.cmd('tcpdump -i h3-eth0 -w logs/scada.pcap &')
     mn.start()
-    h1.cmd('ping -c 1 10.0.0.2')
-    h1.cmdPrint('freeradius -X > logs/radius.log 2>&1 &')
-    h1.cmdPrint('./hostapd hostapd.conf > logs/hostapd.log 2>&1 &')
+    scada.cmd('ping -c 1 10.0.0.2')
+    auth.cmdPrint(
+        'freeradius -t -xx -l logs/radius.log')
+    auth.cmdPrint(
+        './hostapd hostapd.conf -t '
+        '> logs/hostapd.log 2>&1 &')
 
-    h1.cmdPrint('nc -l 10000 > logs/nc.log 2>&1 &')
-    h2.cmdPrint(
-        'wpa_supplicant -i h2-eth0 -D wired -c scada.conf '
-        '> logs/wpa.log 2>&1 &')
+    # auth.cmdPrint('python hostapd_socket.py &')
+    scada.cmdPrint(
+        'wpa_supplicant -i h3-eth0 -D wired -c scada.conf '
+        '-dd -B -f logs/wpa-scada.log')
+    ev.cmdPrint(
+        'wpa_supplicant -i h2-eth0 -D wired -c ev.conf '
+        '-dd -B -f logs/wpa-ev.log')
     # app.exe > logs/save_to.log 2>&1 &
-    h1.cmd('ping -c 1 10.0.0.2')
-    h2.cmdPrint('pkill -2 tcpdump')
+    scada.cmd('ping -c 1 10.0.0.2')
+    s1.cmdPrint('pkill -2 tcpdump')
+    s1.cmdPrint('pkill -2 wpa_supplicant')
+    s1.cmdPrint('pkill -2 hostapd')
+    s1.cmdPrint('pkill -2 freeradius')
     s1.cmd('ovs-ofctl dump-flows s1 > logs/s1.log')
     s2.cmd('ovs-ofctl dump-flows s1 > logs/s2.log')
+    s1.cmdPrint('chmod +r logs/radius.log')
     mn.stop()
 
-
-topos = {'simple_topo': (lambda: Simple_Topology())}
 
 if __name__ == '__main__':
     setLogLevel('info')
