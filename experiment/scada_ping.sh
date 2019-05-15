270f904aa5644a6bfd02f83cd0bd9b463243130d
Arthur Zopellaro <arthurazsoares@gmail.com>
2019-05-15 18:39:03 -0300

    Improve experiment

---
+++ experiment/scada_ping.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#wpa_cli -atst.sh
+IFNAME=$1
+CMD=$2
+
+if [ "$CMD" = "CONNECTED" ]; then
+    echo "$IFNAME Pinging..."
+    ping -c 1 10.0.0.3
+fi
+
+if [ "$CMD" = "DISCONNECTED" ]; then
+    echo "$IFNAME Disconnected..."
+fi
--- experiment/topo.py
+++ experiment/topo.py
@@ -2,31 +2,40 @@ from mininet.topo import Topo
 from mininet.net import Mininet
 from mininet.log import setLogLevel, lg as logger
 from mininet.node import RemoteController
+from time import sleep
 
 
-class Simple_Topology(Topo):
+class Topology(Topo):
     def __init__(self):
         Topo.__init__(self)
 
         h1 = self.addHost(  # TODO Improve
-            'h1', ip='10.0.0.1/24', mac='00:00:00:00:00:01', inNamespace=False)
-        h2 = self.addHost('h2', ip='10.0.0.2/24', mac='00:00:00:00:00:02')
-        h3 = self.addHost('h3', ip='10.0.0.3/24', mac='00:00:00:00:00:03')
+            'h1', inNamespace=False, ip='10.0.0.1', mac='00:00:00:00:00:01')
+        scada = self.addHost(
+            'scada', ip='10.0.0.2', mac='00:00:00:00:00:02')
+        ev = self.addHost(
+            'ev', ip='10.0.0.3', mac='00:00:00:00:00:03')
         s1 = self.addSwitch('s1')
         s2 = self.addSwitch('s2')
 
-        self.addLink(h1, s1)
+        self.addLink(s1, h1)
+        self.addLink(s1, scada)
         self.addLink(s1, s2)
-        self.addLink(s2, h2)
-        self.addLink(s1, h3)
+        self.addLink(s2, ev)
+
+
+def log_sleep(time):
+    logger.info('*** Sleeping for {} seconds...\n'.format(time))
+    sleep(time)
 
 
 def main():
     # app.exe > logs/save_to.log 2>&1 &
-    mn = Mininet(topo=Simple_Topology(), controller=None)
-    mn.addController(
-        'c0', controller=RemoteController, ip='127.0.0.1', port=6653)
-    auth, ev, scada, s1, s2 = mn.get('h1', 'h2', 'h3', 's1', 's2')
+    mn = Mininet(  # TODO Test without static ARP
+        topo=Topology(),  # autoSetMacs=True,
+        autoStaticArp=True,
+        controller=RemoteController('c0', ip='127.0.0.1', port=6653))
+    auth, scada, ev, s1, s2 = mn.get('h1', 'scada', 'ev', 's1', 's2')
 
     s1.cmd('rm -rf /var/run/wpa_supplicant')
 
@@ -42,34 +51,49 @@ def main():
         sw.cmd("sysctl -w net.ipv6.conf.default.disable_ipv6=1")
         sw.cmd("sysctl -w net.ipv6.conf.lo.disable_ipv6=1")
 
-    ev.cmd('tcpdump -i h2-eth0 -w logs/ev.pcap &')
-    auth.cmd('tcpdump -i h1-eth0 -w logs/auth.pcap &')
-    auth.cmd('tcpdump -i lo -w logs/auth-lo.pcap &')
-    s1.cmd('tcpdump -i lo -w logs/s1-lo.pcap &')
-    scada.cmd('tcpdump -i h3-eth0 -w logs/scada.pcap &')
+    s1.cmd('tcpdump -i lo -w logs/openflow.pcap port not 1812 &')
+    s1.cmd('tcpdump -i s1-eth1 -w logs/s1-eth1.pcap &')
+    s1.cmd('tcpdump -i s1-eth2 -w logs/s1-eth2.pcap &')
+    s1.cmd('tcpdump -i s1-eth3 -w logs/s1-eth3.pcap &')
+    auth.cmd('tcpdump -i lo -w logs/radius.pcap port not 6653 &')
+    auth.cmd('tcpdump -i h1-eth0 -w logs/hostapd.pcap &')
+    ev.cmd('tcpdump -i ev-eth0 -w logs/ev.pcap &')
+    scada.cmd('tcpdump -i scada-eth0 -w logs/scada.pcap &')
     mn.start()
 
-    scada.cmd('ping -c 1 10.0.0.2')
+    # sleep(5)
+
+    # scada.cmd('ping -c 1 10.0.0.2')
 
     auth.cmdPrint('freeradius -t -xx -l logs/radius.log')
     auth.cmdPrint('./hostapd hostapd.conf -t > logs/hostapd.log 2>&1 &')
 
-    scada.cmdPrint(
-        'wpa_supplicant -i h3-eth0 -D wired -c scada.conf '
-        '-dd -B -f logs/wpa-scada.log')
+    # sleep(5)
+
     ev.cmdPrint(
-        'wpa_supplicant -i h2-eth0 -D wired -c ev.conf '
+        'wpa_supplicant -i ev-eth0 -D wired -c ev.conf '
         '-dd -B -f logs/wpa-ev.log')
+    log_sleep(.1)
 
-    scada.cmd('ping -c 1 10.0.0.2')
+    scada.cmdPrint(
+        'wpa_supplicant -D wired -c scada.conf -i scada-eth0 '
+        # '-B -f logs/wpa-scada.log ')
+        '-dd -B -f logs/wpa-scada.log ')
 
-    s1.cmdPrint('pkill -2 tcpdump')
+    # sleep(.5)
+    scada.cmdPrint(
+        'wpa_cli -iscada-eth0 -a scada_ping.sh > logs/ping.log 2>&1 &')
+
+    log_sleep(5)
+
+    s1.cmdPrint('pkill -2 wpa_cli')
     s1.cmdPrint('pkill -2 wpa_supplicant')
     s1.cmdPrint('pkill -2 hostapd')
     s1.cmdPrint('pkill -2 freeradius')
-    s1.cmd('ovs-ofctl dump-flows s1 > logs/s1.log')
-    s2.cmd('ovs-ofctl dump-flows s1 > logs/s2.log')
+    s1.cmdPrint('ovs-ofctl dump-flows s1 > logs/s1.log')
+    s2.cmdPrint('ovs-ofctl dump-flows s1 > logs/s2.log')
     s1.cmdPrint('chmod +r logs/radius.log')
+    s1.cmdPrint('pkill -2 tcpdump')
     mn.stop()
 
 
