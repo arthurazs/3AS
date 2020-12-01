b8f445e777cfc0164481db426f903fb0bc9c4fb7
Arthur Zopellaro <arthurazsoares@gmail.com>
2020-12-01 18:11:24 -0300

    Update time_delay graph

--- journal/time_delay.py
+++ journal/time_delay.py
@@ -30,7 +30,10 @@ def load_csv(name, proto=None, evs=1, rep=1):
         '_ws.col.Destination': 'destination',
         '_ws.col.Protocol': 'protocol',
         '_ws.col.Length': 'length',
-        '_ws.col.Info': 'information'})
+        '_ws.col.Info': 'information',
+        'eapol.type': 'eapol_type',
+        'eth.src': 'eth_src',
+    })
 
     if name == 'sdn-hostapd':
         data_frame.loc[data_frame['protocol'] == 'TCP', 'Traffic'] = 'API'
@@ -76,18 +79,63 @@ def normalize_time(data_frame):
     return data_set
 
 
+def debug(df):
+    print(df.to_string())
+    print()
+    print(df)
+    exit()
+
+
+def helper(data_frame, query):
+    row = data_frame.query(query).values[0]
+    value = row[-1]
+    address = row[0]
+    new_data_frame = data_frame.query('source != @address or destination != @address')
+    return value, address, new_data_frame
+
+
+# def ip_adder(value):  # INCORRECT!
+#     # IP Address Adder for netmask /16
+#     rest, network, host = value.rsplit('.', 2)
+#     host = (int(host) % 254) + 1
+#     network = int(network) + int(host == 1)
+#     return rest + '.' + str(network) + '.' + str(host)
+
+
+def mac_adder(value):  # INCORRECT!
+    # MAC Address Adder
+    rest, network, host = value.rsplit(':', 2)
+    host = (int(host, 16) % 254) + 1
+    network = int(network, 16) + int(host == 1)
+    host = hex(host)[2:].zfill(2).lower()
+    network = hex(network)[2:].zfill(2).lower()
+    return rest + ':' + network + ':' + host
+
+
+# mac2ip = {}
+# previous_mac = '00:00:00_00:00:03'
+# previous_ip = '10.0.1.3'
+# for _ in range(4, 1004):
+#
+#     # TODO we have to use the wrong func here bc our experiment is already done
+#     # TODO                  f'00:00:00:00:{index // 256:02x}:{index % 256:02x}'
+#     mac = mac_adder(previous_mac)  # TODO this mac_adder is incorrect, use above
+#     ip = ip_adder(previous_ip)  # TODO same as above, ip_adder is incorrect
+#     mac2ip[mac] = ip
+#     previous_mac, previous_ip = mac, ip
+
+
 def load(evs, rep, verbose=False):
     if verbose:
         print('loading datasets...')
-    radius = load_csv('freeradius', 'RADIUS', evs, rep)
+    # radius = load_csv('freeradius', 'RADIUS', evs, rep)
     openflow = load_csv('openflow', 'OpenFlow', evs, rep)
     scada = load_csv('scada', 'MMS', evs, rep)
     hostapd = load_csv('sdn-hostapd', 'EAPoL', evs, rep)
     if verbose:
         print('loaded\n')
         print('concatenating datasets...')
-    auth_tuple = (radius, openflow,  scada,
-                  hostapd)
+    auth_tuple = (openflow, scada, hostapd)
     authentication = pd.concat(auth_tuple)
     if verbose:
         print('concatenated\n')
@@ -103,33 +151,44 @@ def load(evs, rep, verbose=False):
         print('normalized\n')
         print('sampling down...')
         print(authentication.shape)
-    end = -1
-    this_rep = pd.DataFrame()
-    authentication = authentication.query(
-        'Traffic != "OpenFlow" or (Traffic == "OpenFlow" and information.str.contains("Type: "))')
-    while not authentication.empty:
-        try:
-            helper = authentication.query('Traffic == "EAPoL" and information == "Start"').values[0][-1]
-        except IndexError:
-            helper = authentication.values[-1][-1]
-        start = authentication.query('time < @helper').query(
-            'Traffic == "OpenFlow" and information == "Type: OFPT_PACKET_IN"').values[0][-1]
-        helper2 = authentication[authentication.Traffic == 'MMS'].values[0]
-        end = helper2[-1]
-        ip = helper2[0]
-        authentication = authentication.query('source != @ip')
-        this_rep['delay'] = [end - start]  # TODO media de todos veiculos no experimento + media por experimento? e se colocar a media separada por veiculo num grafico de pontos e tals
-        authentication = authentication.query('time > @helper')
 
-    # return partial
-    # return partial.groupby('Traffic').sum()
-    return this_rep.mean()
+    this_rep = pd.DataFrame()
+    current_ev = 0
+    current_mac = '00:00:00:00:00:03'
+    for _ in range(4, evs + 4):
+        current_mac = mac_adder(current_mac)
+        # current_ip = mac2ip[current_mac]
+
+        openflow_start = authentication.query(
+            'Traffic == "OpenFlow" and eapol_type == 1 and information == "Type: OFPT_PACKET_IN" '
+            f'and eth_src == "00:00:00:00:00:00,{current_mac}"')['time'].values[0]
+        mms_start = authentication.query(
+            'Traffic == "MMS" and length == 0.074 and protocol == "TCP" and '
+            f'eth_src == "{current_mac}"')['time'].values[0]
+        # eapol_start, _, authentication = helper(
+        #     authentication, f'Traffic == "EAPoL" and source == "{current_mac}"')
+        # mms_start, _, authentication = helper(
+        #     authentication, f'Traffic == "MMS" and source == "{current_ip}"')
+
+        aux = pd.DataFrame()
+        current_ev += 1
+        aux['evs'] = [evs]
+        aux['rep'] = [rep]
+        aux['ev'] = [current_ev]
+        aux['start'] = [openflow_start]
+        aux['end'] = [mms_start]
+        aux['delay'] = [mms_start - openflow_start]
+        this_rep = this_rep.append(aux, ignore_index=True)
+        # TODO media de todos veiculos no experimento + media por experimento?
+        # TODO e se colocar a media separada por veiculo num grafico de pontos e tals
+    # return this_rep
+    return this_rep.sort_values(by='start')
 
 
 def load_all():
-    dataset = pd.DataFrame()
+    loaded_dataset = pd.DataFrame()
+    # for ev in [1, 10, 300, 1000]:
     for ev in [1000]:
-        # for ev in [1, 10, 300, 1000]:
         for reps in range(1, 11):
             print(f'loading {ev}_{reps}...')
             q = load(ev, reps, verbose=False)
@@ -142,8 +201,8 @@ def load_all():
                 aux['evs'] = [ev]
                 aux['length'] = [length]
                 aux['Traffic'] = [name]
-                dataset = dataset.append(aux, ignore_index=True)
-    return dataset
+                loaded_dataset = loaded_dataset.append(aux, ignore_index=True)
+    return loaded_dataset
 
 
 # === CODE ===
