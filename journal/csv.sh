0ecde4d8d9c92375e1c4d09fb7f05e944cc122bd
Arthur Zopellaro <arthurazsoares@gmail.com>
2020-12-01 18:11:02 -0300

    Add eapol info to CSV

--- journal/csv.sh
+++ journal/csv.sh
@@ -4,13 +4,13 @@ for evs in 1 10 300 1000
 do
     for rep in $(seq 1 10)
     do
-        folder=$prefix$evs\_$rep/pcap
+        folder="$prefix""$evs"_"$rep"/pcap
         echo
-        echo Parsing $folder to csv...
+        echo Parsing "$folder" to csv...
         for file in scada sdn-hostapd freeradius openflow
         do
             echo \> Parsing $file...
-            tshark -r $folder/$file.pcap -T fields -e frame.time_epoch -e _ws.col.Source -e _ws.col.Destination  -e _ws.col.Protocol -e _ws.col.Length -e _ws.col.Info -E header=y -E separator=, -E quote=d > $folder/$file.csv
+            tshark -r "$folder"/$file.pcap -T fields -e frame.time_epoch -e _ws.col.Source -e _ws.col.Destination  -e _ws.col.Protocol -e _ws.col.Length -e _ws.col.Info -e eapol.type -e eth.src -E header=y -E separator=, -E quote=d > "$folder"/$file.csv
         done
     done
 done
